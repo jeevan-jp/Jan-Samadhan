26cd8f5681654bbf4f9c4456b8e3948809e4080b
jeevan-jp <jiwanprakash65@gmail.com>
2018-06-22 20:32:37 +0530

    Updated home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { WebService } from '../../web.service'
 import { Complaint } from '../../models/complaints';
 import { flyInOut, expand, glow } from '../../animations/app.animations';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -11,18 +12,21 @@ import { flyInOut, expand, glow } from '../../animations/app.animations';
     glow()
   ]
 })
+
 export class HomeComponent implements OnInit {
+
   constructor( private webService: WebService ) {}
+
   complaints:Complaint[];
-  five: Complaint[];
+  six: Complaint[];
+
   async ngOnInit() {
     const response = await this.webService.getMessages();
     console.log(response.json());
 
-
     this.complaints = response.json();
-    this.five = this.complaints.slice(this.complaints.length - 5);
-    console.log(this.five);
+    this.six = this.complaints.slice(this.complaints.length - 6);
+    console.log(this.six);
   }
 
 }
