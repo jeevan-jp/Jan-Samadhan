37fab4e5937dc82b2e4319cfd5dfcff0e9383a9d
mohit <mjain8156@gmail.com>
2018-06-22 15:26:18 +0530

    Add your complaints components

---
+++ src/app/components/your-complaints/your-complaints.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-your-complaints',
+  templateUrl: './your-complaints.component.html',
+  styleUrls: ['./your-complaints.component.css']
+})
+export class YourComplaintsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
